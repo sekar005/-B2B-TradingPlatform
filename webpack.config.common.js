cfef776134356f875e8ac83016cda15cc85bc56d
hhagmans <hhagmans@users.noreply.github.com>
2016-11-03 11:27:04 +0100

    Common Webpack config

---
+++ webpack.config.common.js
@@ -0,0 +1,15 @@
+module.exports = {
+     entry: './src/client/main.js',
+     output: {
+         path: './public/dist',
+         filename: 'app.js',
+     },
+     module: {
+         loaders: [{
+             test: /\.js$/,
+             exclude: /node_modules/,
+             loaders: [ "babel-loader", "eslint-loader" ]
+         }]
+     }
+     devtool: 'source-map'
+ }
