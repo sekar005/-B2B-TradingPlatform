1dba6ac186221e49f6daf2172c1bc2175a370c8c
Sebastian Karrie <sebastiankarrie@dokom.net>
2016-11-03 11:46:52 +0100

    Add configurations
    Webpack configurations for development and production added

---
+++ webpack.config.prod.js
@@ -0,0 +1,14 @@
+var webpackMerge = require('webpack-merge');
+var commonConfig = require('./webpack.common.js');
+
+module.exports = webpackMerge(commonConfig, {
+	plugins:[
+		new webpack.NoErrorsPlugin(),
+		new webpack.OccurenceOrderPlugin(),
+		new webpack.DefinePlugin({'process.env': { 
+			'NODE_ENV': '"production"'
+			'API_SERVER_URL':'"http://web2.de:8888"'
+		}}),
+		new webpack.UglifyJsPlugin()
+	]
+})
