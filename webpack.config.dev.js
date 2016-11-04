6b5ce594aa0428f4553d7b1dcae6704bd6d5f432
hhagmans <hhagmans@users.noreply.github.com>
2016-11-04 11:53:15 +0100

    Fixed a lot of errors

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,13 +1,14 @@
+var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
-var commonConfig = require('./webpack.common.js');
+var commonConfig = require('./webpack.config.common.js');
 
 module.exports = webpackMerge(commonConfig, {
 	plugins:[
 		new webpack.NoErrorsPlugin(),
-		new webpack.OccurenceOrderPlugin(),
+		new webpack.optimize.OccurrenceOrderPlugin(),
 		new webpack.DefinePlugin({'process.env': { 
-			'NODE_ENV': '"production"'
-			'API_SERVER_URL':'"http://web2.de:8888"'
+			'NODE_ENV': '"production"',
+			'API_SERVER_URL':'"http://localhost:3000"'
 		}}),
-		new webpack.UglifyJsPlugin()
-})
+		new webpack.optimize.UglifyJsPlugin(),
+]})
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,14 +1,15 @@
+var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
-var commonConfig = require('./webpack.common.js');
+var commonConfig = require('./webpack.config.common.js');
 
 module.exports = webpackMerge(commonConfig, {
 	plugins:[
 		new webpack.NoErrorsPlugin(),
-		new webpack.OccurenceOrderPlugin(),
+		new webpack.optimize.OccurrenceOrderPlugin(),
 		new webpack.DefinePlugin({'process.env': { 
 			'NODE_ENV': '"production"'
 			'API_SERVER_URL':'"http://web2.de:8888"'
 		}}),
-		new webpack.UglifyJsPlugin()
+		new webpack.optimize.UglifyJsPlugin(),
 	]
 })
