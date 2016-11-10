8eb10e59f87c5529b0655ac8d84d2f9cd0331fa5
hhagmans <hhagmans@users.noreply.github.com>
2016-11-10 11:09:42 +0100

    Fixed prod config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = webpackMerge(commonConfig, {
 		new webpack.NoErrorsPlugin(),
 		new webpack.optimize.OccurrenceOrderPlugin(),
 		new webpack.DefinePlugin({'process.env': { 
-			'NODE_ENV': '"production"'
+			'NODE_ENV': '"production"',
 			'API_SERVER_URL':'"http://web2.de:8888"'
 		}}),
 		new webpack.optimize.UglifyJsPlugin(),
