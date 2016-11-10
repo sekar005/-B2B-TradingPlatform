eba8ce49b3fcfb57044e5253d7799b9b69974f13
Pfeiffer <tim.pfeifer@adesso.local>
2016-11-10 11:08:59 +0100

    function for Babel test

--- src/client/main.js
+++ src/client/main.js
@@ -1,3 +1,10 @@
 var apiUrl = process.env.API_SERVER_URL;
 var environment = process.env.NODE_ENV;
 document.write(apiUrl + ' ' + environment);
+function test() {
+	if(3>2) {
+		let i=2;
+		i++;
+	}
+}
+test();
