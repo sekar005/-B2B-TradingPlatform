a0d12f8b1133ea06d1ba278fe581f2e5466e60bc
hhagmans <hhagmans@users.noreply.github.com>
2016-11-24 10:43:01 +0100

    Add ES2016 power example

--- src/client/main.js
+++ src/client/main.js
@@ -2,6 +2,7 @@ var apiUrl = process.env.API_SERVER_URL;
 var environment = process.env.NODE_ENV;
 document.write(apiUrl + ' ' + environment);
 function test() {
+	2 ** 3;
 	if(3>2) {
 		let i=2;
 		i++;
