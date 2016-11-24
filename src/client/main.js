8445e5a2d5c35ad739bef2308dcaea71e586e7a2
hhagmans <hhagmans@users.noreply.github.com>
2016-11-24 11:04:50 +0100

    Last version of test file

--- src/client/main.js
+++ src/client/main.js
@@ -2,10 +2,7 @@ var apiUrl = process.env.API_SERVER_URL;
 var environment = process.env.NODE_ENV;
 document.write(apiUrl + ' ' + environment);
 function test() {
-	2 ** 3;
-	if(3>2) {
-		let i=2;
-		i++;
-	}
+	let a = 2 ** 3;
+	console.log(a);
 }
 test();
